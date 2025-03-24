baf1ccd49cdf02fa67ed5dbdfb82c409c5d6194f
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-03-24 12:30:58 +0000

    placeholder for our cnn architecture

---
+++ cnn.py
@@ -0,0 +1,18 @@
+from tensorflow.keras import Sequential
+
+
+def CNN(input_shape, dropout):
+    """
+    Convolutional neural network for the classification of gravitation wave signals.
+
+    Structure
+    ---------
+    Input
+
+    Output
+    """
+    model = Sequential()
+
+    # Output
+
+    return model
