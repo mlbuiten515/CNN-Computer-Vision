20318d813e713a015b589945294cbfc7fdd51d8c
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-03-25 17:33:42 +0000

    created a class to call for data processing

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,42 @@
-import numpy as np
+import os
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
 
+
+class ImagesDataset(Dataset):
+    def __init__(self, data_dir,
+                 transform=transforms.Compose([transforms.ToTensor()])):
+
+        self.data_dir = data_dir 
+        self.class_set = sorted(os.listdir(data_dir))
+        self.transform = transform
+
+        self.image_paths = []
+        self.labels = []
+
+        self.class_encode = {cls: i for i, cls in enumerate(self.class_set)}
+
+        for cls in self.class_set:
+            dir = os.path.join(data_dir, cls)
+            for img in os.listdir(dir):
+                self.image_paths.append(os.path.join(dir, img))
+                self.labels.append(self.class_encode[cls])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        img_label = self.labels[idx]
+
+        try:
+            image = Image.open(img_path).convert('RGB')
+        except Exception as e:
+            print(f'Error loading {img_path}: {e}')
+            return None, None
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, img_label
