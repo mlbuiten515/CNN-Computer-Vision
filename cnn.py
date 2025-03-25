201f5bb8fa8404ade5b90612c46b4defaeb87e85
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-03-25 17:37:31 +0000

    input basic CNN to placehold for now

--- cnn.py
+++ cnn.py
@@ -1,19 +1,42 @@
-from tensorflow.keras import Sequential
+from torch import nn
+import torch.nn.functional as F
 
 
-def CNN(input_shape, dropout):
-    """
-    Convolutional neural network for the classification
-    of UCMerced LanUsed dataset
+# define the model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
 
-    Structure
-    ---------
-    Input
+        self.conv1 = nn.Conv2d(1,10,5)    # Convolutional layer
+        self.bn1 = nn.BatchNorm2d(10)     # batch normalization
+        self.mp1 = nn.MaxPool2d(2, 2)     # Max Pooling
 
-    Output
-    """
-    model = Sequential()
+        self.conv2 = nn.Conv2d(10,20,3)
+        self.bn2 = nn.BatchNorm2d(20)
 
-    # Output
+        self.fc1 = nn.Linear(20*10*10,500)
+        self.fc2 = nn.Linear(500, 10)
 
-    return model
+    def forward(self, x):
+        input_size = x.size(0)
+        # in: batch*1*28*28, out: batch*10*24*24  -- (28-5+1)
+        x = self.conv1(x)
+        x = F.relu(x)  # does not change the input size
+        x = self.bn1(x)  # does not change the input size
+        # in: batch*10*24*24, out: batch*10*12*12
+        x = self.mp1(x)
+
+        # in: batch*10*12*12, out: batch*20*10*10  -- (12-3+1)
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        # 20*10*10 = 2000
+        x = x.view(input_size,-1)
+
+        # in: batch*2000  out:batch*500
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        # in:batch*500 out:batch*10
+        x = self.fc2(x)
+        return x
