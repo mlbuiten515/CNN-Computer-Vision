dad25d23685906e2cd8ecb46b82b1ac0be67814b
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-03-26 20:11:30 +0000

    completed first cnn arch

--- cnn.py
+++ cnn.py
@@ -1,5 +1,4 @@
 from torch import nn
-import torch.nn.functional as F
 
 
 # define the model
@@ -7,36 +6,61 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 10, 5)    # Convolutional layer
-        self.bn1 = nn.BatchNorm2d(10)     # batch normalization
-        self.mp1 = nn.MaxPool2d(2, 2)     # Max Pooling
+        # input 256 x 256 x 3
 
-        self.conv2 = nn.Conv2d(10, 20, 3)
-        self.bn2 = nn.BatchNorm2d(20)
+        # Layer 1
+        self.conv1 = nn.Conv2d(3, 16, 3, padding='same')
+        self.act = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(16)
+        self.mp1 = nn.MaxPool2d(2, 2)  # output: 128 x 128 x 16
 
-        self.fc1 = nn.Linear(20*10*10, 500)
-        self.fc2 = nn.Linear(500, 10)
+        # Layer 2
+        self.conv2 = nn.Conv2d(16, 32, 3, padding='same', dilation=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        # output: 64 x 64 x 32
+
+        # Layer 3
+        self.conv3 = nn.Conv2d(32, 64, 3, padding='same')
+        self.bn3 = nn.BatchNorm2d(64)
+        # output: 32 x 32 x 64
+
+        # Layer 4
+        self.conv4 = nn.Conv2d(64, 128, 3, padding='same')
+        self.bn4 = nn.BatchNorm2d(128)
+        # output: 16 x 16 x 128
+
+        # Layer Connecting
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(16*16*128, 512)
+        self.fc2 = nn.Linear(512, 21)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        input_size = x.size(0)
-        # in: batch*1*28*28, out: batch*10*24*24  -- (28-5+1)
+
         x = self.conv1(x)
-        x = F.relu(x)  # does not change the input size
-        x = self.bn1(x)  # does not change the input size
-        # in: batch*10*24*24, out: batch*10*12*12
+        x = self.act(x)
+        x = self.bn1(x)
         x = self.mp1(x)
 
-        # in: batch*10*12*12, out: batch*20*10*10  -- (12-3+1)
         x = self.conv2(x)
-        x = F.relu(x)
+        x = self.act(x)
+        x = self.bn2(x)
+        x = self.mp1(x)
 
-        # 20*10*10 = 2000
-        x = x.view(input_size, -1)
+        x = self.conv3(x)
+        x = self.act(x)
+        x = self.bn3(x)
+        x = self.mp1(x)
 
-        # in: batch*2000  out:batch*500
+        x = self.conv4(x)
+        x = self.act(x)
+        x = self.bn4(x)
+        x = self.mp1(x)
+
+        x = self.flatten(x)
+
         x = self.fc1(x)
-        x = F.relu(x)
-
-        # in:batch*500 out:batch*10
         x = self.fc2(x)
+        x = self.softmax(x)
+
         return x
